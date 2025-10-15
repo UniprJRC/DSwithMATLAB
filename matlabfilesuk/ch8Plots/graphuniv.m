c4b8ced744bfd6b4fb46bb433d98069ec291e6f6
UniprJRC <FSDA@unipr.it>
2025-10-15 15:39:46 +0000

    🔄 synced local 'matlabfilesuk/' with remote 'matlabMfilesUK/'

--- matlabfilesuk/ch8Plots/graphuniv.m
+++ matlabfilesuk/ch8Plots/graphuniv.m
@@ -33,3 +33,38 @@ title('Stem plot')
 
 
 
+%%
+figure
+% Create a tiled layout with 2 rows and 3 columns
+tiledlayout('flow');
+
+% Create the first plot
+nexttile
+bar(cats, y, 'BaseValue', m);
+title('Vertical bars');
+
+% Create the second plot
+nexttile
+barh(cats, y, 'BaseValue', m);
+title('Horizontal bars');
+
+% Create the third plot
+nexttile
+plot(cats, y);
+title('Line plot');
+
+% Create the fourth plot
+nexttile
+area(cats, y);
+title('Area plot');
+
+% Create the fifth plot
+nexttile
+stairs(cats, y);
+title('Stairs plot');
+
+% Create the sixth plot
+nexttile
+stem(cats, y);
+title('Stem plot');
+sgtitle('With tiledlayout implementation')
