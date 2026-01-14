c5eb7798425222be2f56f240f8318954610e2b99
UniprJRC <FSDA@unipr.it>
2026-01-14 09:32:02 +0000

    🔄 synced local 'matlabfilesuk/' with remote 'matlabMfilesUK/'

--- matlabfilesuk/ch2Algebra/projectionIntro.m
+++ matlabfilesuk/ch2Algebra/projectionIntro.m
@@ -7,7 +7,7 @@ hatx = t*v;
 dist = norm(x-hatx);
 disp(['Distance from x to line: ' num2str(dist)])
 
-projlen = norm(hatx);
+projlen = norm(hatx); % abs(x'*v)/(norm(v))
 disp(['Projection length: ' num2str(projlen)])
 
 disp('Check orthogonality v''*(x-hatx):')
@@ -22,14 +22,16 @@ text(v(1),v(2),v(3),'v','FontSize',fs)
 quiver3(0,0,0,t*v(1),t*v(2),t*v(3),0,'k')
 
 % Remark: the dx values were entered manually to prevent
-% the text from overlapping the vector (not the book)
+% the text from overlapping the vector 
 dx=[-0.1 -0.4 0];
-text(t*v(1)+dx(1),t*v(2)+dx(2),t*v(3)+dx(3), ['$\hat x=tv=' num2str(t) '*v$'], ...
+t1=t*v(1)+dx(1); t2=t*v(2)+dx(2); t3=t*v(3)+dx(3);
+text(t1,t2,t3, ['$\hat x=tv=' num2str(t) '*v$'], ...
     'FontSize',fs,'Rotation',-5, 'Interpreter','latex');
 
 quiver3(x(1),x(2),x(3),-x(1)+hatx(1),-x(2)+hatx(2),-x(3)+hatx(3),0,'r')
 dy=0.15;
-text(x(1),x(2),x(3)+dy,'$x-\hat x$','FontSize',fs,'Rotation',-57,'Interpreter','latex')
+text(x(1),x(2),x(3)+dy,'$x-\hat x$','FontSize',fs, ...
+    'Rotation',-57,'Interpreter','latex')
 xlabel('First coordinate'); 
 ylabel('Second coordinate'); 
 zlabel('Third coordinate')
