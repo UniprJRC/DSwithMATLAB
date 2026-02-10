ee439ed43ff2a920493f13f0460e333bc2797df3
UniprJRC <FSDA@unipr.it>
2026-02-10 15:39:35 +0000

    🔄 synced local 'matlabfilesuk/' with remote 'matlabMfilesUK/'

--- matlabfilesuk/ch11PCA/pcaGeographic.m
+++ matlabfilesuk/ch11PCA/pcaGeographic.m
@@ -1,13 +1,15 @@
 %% Ex with lat e long
-load citiesItaly2024.mat0
+load citiesItaly2024.mat
 % Retrieve Latitude and Longitude of each province
 LatLong=citiesItaly2024.Properties.UserData{2};
+% Call to pcaFS with options 'Latitude' and 'Longitude'
 out=pcaFS(citiesItaly2024,'Latitude',LatLong(:,1),'Longitude',LatLong(:,2));
 
 %% Ex with shape
 load citiesItaly2024.mat
 % Retrieve Shape file
 ShapeFile=citiesItaly2024.Properties.UserData{1};
+% Call to pcaFS with option 'ShapeFile' 
 out=pcaFS(citiesItaly2024,'ShapeFile',ShapeFile);
 
 
